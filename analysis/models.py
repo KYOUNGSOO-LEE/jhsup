901429c05ea0194733cb4accc089b2ed10976506
Kyoungsoo Lee <ksteach81@gmail.com>
2019-10-06 12:40:24 +0900

    model/chain(by ForeignKey)

--- analysis/models.py
+++ analysis/models.py
@@ -1,17 +1,97 @@
 from django.db import models
 
 
+class UnivGroup(models.Model):
+    univ_group = models.CharField(max_length=3)  # 일반대/특수대/산업대
+
+    def __str__(self):
+        return self.univ_group
+
+
+class UnivRegion(models.Model):
+    univ_region = models.CharField(max_length=2)  # 대학소재
+
+    def __str__(self):
+        return self.univ_region
+
+
+class MajorGroup(models.Model):
+    major_group = models.CharField(max_length=3)  # 계열
+
+    def __str__(self):
+        return self.major_group
+
+
+class UnivName(models.Model):
+    univ_name = models.CharField(max_length=10)  # 대학명
+    univ_group = models.ForeignKey('UnivGroup', on_delete=models.CASCADE)  # 일반대/특수대/산업대
+    univ_region = models.ForeignKey('UnivRegion', on_delete=models.CASCADE)  # 대학소재
+
+    def __str__(self):
+        return self.univ_name
+
+
+class UnivMajor(models.Model):
+    univ_major = models.CharField(max_length=10)  # 모집단위
+    univ_group = models.ForeignKey('UnivGroup', on_delete=models.CASCADE)  # 일반대/특수대/산업대
+    univ_region = models.ForeignKey('UnivRegion', on_delete=models.CASCADE)  # 대학소재
+    major_group = models.ForeignKey('MajorGroup', on_delete=models.CASCADE)  # 계열
+    univ_name = models.ForeignKey('UnivName', on_delete=models.CASCADE)  # 대학명
+
+    def __str__(self):
+        return self.univ_major
+
+
+class Admission1(models.Model):
+    admission1 = models.CharField(max_length=2)  # 전형유형(교과/논술/실시/종합)
+    univ_group = models.ForeignKey('UnivGroup', on_delete=models.CASCADE)  # 일반대/특수대/산업대
+    univ_region = models.ForeignKey('UnivRegion', on_delete=models.CASCADE)  # 대학소재
+    major_group = models.ForeignKey('MajorGroup', on_delete=models.CASCADE)  # 계열
+    univ_name = models.ForeignKey('UnivName', on_delete=models.CASCADE)  # 대학명
+    univ_major = models.ForeignKey('UnivMajor', on_delete=models.CASCADE)  # 모집단위
+
+    def __str__(self):
+        return self.admission1
+
+
+class Admission2(models.Model):
+    admission2 = models.CharField(max_length=2)  # 전형유형(교과/논술/실시/종합)
+    univ_group = models.ForeignKey('UnivGroup', on_delete=models.CASCADE)  # 일반대/특수대/산업대
+    univ_region = models.ForeignKey('UnivRegion', on_delete=models.CASCADE)  # 대학소재
+    major_group = models.ForeignKey('MajorGroup', on_delete=models.CASCADE)  # 계열
+    univ_name = models.ForeignKey('UnivName', on_delete=models.CASCADE)  # 대학명
+    univ_major = models.ForeignKey('UnivMajor', on_delete=models.CASCADE)  # 모집단위
+    admission1 = models.ForeignKey('Admission1', on_delete=models.CASCADE)  # 전형유형(교과/논술/실시/종합)
+
+    def __str__(self):
+        return self.admission2
+
+
+class Admission3(models.Model):
+    admission3 = models.CharField(max_length=2)  # 전형유형(교과/논술/실시/종합)
+    univ_group = models.ForeignKey('UnivGroup', on_delete=models.CASCADE)  # 일반대/특수대/산업대
+    univ_region = models.ForeignKey('UnivRegion', on_delete=models.CASCADE)  # 대학소재
+    major_group = models.ForeignKey('MajorGroup', on_delete=models.CASCADE)  # 계열
+    univ_name = models.ForeignKey('UnivName', on_delete=models.CASCADE)  # 대학명
+    univ_major = models.ForeignKey('UnivMajor', on_delete=models.CASCADE)  # 모집단위
+    admission1 = models.ForeignKey('Admission1', on_delete=models.CASCADE)  # 전형유형(교과/논술/실시/종합)
+    admission2 = models.ForeignKey('Admission2', on_delete=models.CASCADE)  # 세부유형(일반전형/농어촌학생 등)
+
+    def __str__(self):
+        return self.admission3
+
+
 class Student(models.Model):
     entrance_year = models.IntegerField()  # 입시학년도
     student_region = models.CharField(max_length=3)  # 수합지역
-    univ_group = models.CharField(max_length=3)  # 일반대/특수대/산업대
-    univ_region = models.CharField(max_length=2)  # 대학소재
-    major_group = models.CharField(max_length=3)  # 계열
-    univ_name = models.CharField(max_length=10)  # 대학명
-    univ_major = models.CharField(max_length=10)  # 모집단위
-    admission1 = models.CharField(max_length=2)  # 전형유형(교과/논술/실시/종합)
-    admission2 = models.CharField(max_length=20)  # 세부유형(일반전형/농어촌학생 등)
-    admission3 = models.CharField(max_length=10)  # 선발유형(일괄합산/1단계 등)
+    univ_group = models.ForeignKey('UnivGroup', on_delete=models.CASCADE)  # 일반대/특수대/산업대
+    univ_region = models.ForeignKey('UnivRegion', on_delete=models.CASCADE)  # 대학소재
+    major_group = models.ForeignKey('MajorGroup', on_delete=models.CASCADE)  # 계열
+    univ_name = models.ForeignKey('UnivName', on_delete=models.CASCADE)  # 대학명
+    univ_major = models.ForeignKey('UnivMajor', on_delete=models.CASCADE)  # 모집단위
+    admission1 = models.ForeignKey('Admission1', on_delete=models.CASCADE)  # 전형유형(교과/논술/실시/종합)
+    admission2 = models.ForeignKey('Admission2', on_delete=models.CASCADE)  # 세부유형(일반전형/농어촌학생 등)
+    admission3 = models.ForeignKey('Admission2', on_delete=models.CASCADE)  # 선발유형(일괄합산/1단계 등)
     grade = models.DecimalField(decimal_places=3, max_digits=4)  # 등급(대학별 환산등급)
     univ_score = models.DecimalField(decimal_places=3, max_digits=10, null=True)  # 대학별 환산점수
     korean = models.DecimalField(decimal_places=3, max_digits=4)  # 국어
