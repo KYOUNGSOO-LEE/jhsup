70a9d14ef0aa424d1d059ac36abaf25234341686
Kyoungsoo Lee <ksteach81@gmail.com>
2019-10-06 21:57:55 +0900

    19100521

--- analysis/views.py
+++ analysis/views.py
@@ -261,93 +261,6 @@ def search3(request):
 
 
 @permission_required('admin.can_add_log_entry')
-def univ_data_upload(request):
-    template = "student_data_upload.html"
-
-    prompt = {
-        'order': 'student_data.csv uploader'
-    }
-
-    if request.method == 'GET':
-        return render(request, template, prompt)
-
-    csv_file = request.FILES['file']
-
-    if not csv_file.name.endswith('.csv'):
-        messages.error(request, 'This is not a csv file')
-
-    data_set = csv_file.read().decode('UTF-8')
-    io_string = io.StringIO(data_set)
-    next(io_string)
-    for column in csv.reader(io_string, delimiter=',', quotechar="|"):
-
-        if column[11] == '' or column[11] == '0':
-            column[11] = None
-
-        if column[40] == '' or column[40] == '0':
-            column[40] = None
-
-        if column[42] == '' or column[42] == '0':
-            column[42] = None
-
-        if column[43] == '' or column[43] == '0':
-            column[43] = None
-
-        if column[44] == '' or column[44] == '0':
-            column[44] = None
-
-        _, created = Student.objects.update_or_create(
-            entrance_year=column[0],
-            student_region=column[1],
-            univ_group=column[2],
-            univ_region=column[3],
-            major_group=column[4],
-            univ_name=column[5],
-            univ_major=column[6],
-            admission1=column[7],
-            admission2=column[8],
-            admission3=column[9],
-            grade=column[10],
-            univ_score=column[11],
-            korean=column[12],
-            english=column[13],
-            mathematics=column[14],
-            society=column[15],
-            science=column[16],
-            all_subject_100=column[17],
-            all_subject_244=column[18],
-            all_subject_433=column[19],
-            all_subject_235=column[20],
-            ko_en_math_soc_sci_100=column[21],
-            ko_en_math_soc_sci_244=column[22],
-            ko_en_math_soc_sci_334=column[23],
-            ko_en_math_soc_sci_433=column[24],
-            ko_en_math_100=column[25],
-            ko_en_math_soc_100=column[26],
-            ko_en_math_soc_244=column[27],
-            ko_en_math_soc_334=column[28],
-            ko_en_math_soc_433=column[29],
-            ko_en_math_soc_370=column[30],
-            ko_en_soc_100=column[31],
-            ko_en_soc_244=column[32],
-            ko_en_math_sci_100=column[33],
-            ko_en_math_sci_244=column[34],
-            ko_en_math_sci_334=column[35],
-            ko_en_math_sci_433=column[36],
-            ko_en_math_sci_370=column[37],
-            en_math_sci_100=column[38],
-            en_math_sci_244=column[39],
-            first_step=column[40],
-            final_step=column[41],
-            fail_reason=column[42],
-            candidate_rank=column[43],
-            personnel=column[44]
-        )
-    context = {}
-    return render(request, template, context)
-
-
-@permission_required('admin.can_add_log_entry')
 def univ_data_upload1(request):
     template = "univ_data_upload.html"
 
@@ -619,5 +532,119 @@ def univ_data_upload7(request):
             admission2=admission2_id
         )
 
+    context = {}
+    return render(request, template, context)
+
+
+@permission_required('admin.can_add_log_entry')
+def univ_data_upload8(request):
+    template = "univ_data_upload.html"
+
+    prompt = {
+        'order': 'student_data.csv uploader(Student_data upload)'
+    }
+
+    if request.method == 'GET':
+        return render(request, template, prompt)
+
+    csv_file = request.FILES['file']
+
+    if not csv_file.name.endswith('.csv'):
+        messages.error(request, 'This is not a csv file')
+
+    data_set = csv_file.read().decode('UTF-8')
+    io_string = io.StringIO(data_set)
+    next(io_string)
+
+    # Student_data upload
+    univ_group_qs = UnivGroup.objects.all()
+    univ_region_qs = UnivRegion.objects.all()
+    major_group_qs = MajorGroup.objects.all()
+    univ_name_qs = UnivName.objects.all()
+    univ_major_qs = UnivMajor.objects.all()
+    admission1_qs = Admission1.objects.all()
+    admission2_qs = Admission2.objects.all()
+    admission3_qs = Admission3.objects.all()
+    personnel_qs = Personnel.objects.all()
+    for column in csv.reader(io_string, delimiter=',', quotechar="|"):
+        univ_group_id = univ_group_qs.filter(univ_group=column[2])[0]
+        univ_region_id = univ_region_qs.filter(univ_region=column[3])[0]
+        major_group_id = major_group_qs.filter(major_group=column[4])[0]
+        univ_name_id = univ_name_qs.filter(univ_name=column[5])[0]
+        univ_major_id = univ_major_qs.filter(univ_major=column[6]).filter(univ_name_id=univ_name_id)[0]
+        admission1_id = admission1_qs.filter(admission1=column[7]).filter(univ_major_id=univ_major_id) \
+            .filter(univ_name_id=univ_name_id)[0]
+        admission2_id = admission2_qs.filter(admission2=column[8]).filter(admission1_id=admission1_id) \
+            .filter(univ_major_id=univ_major_id).filter(univ_name_id=univ_name_id)[0]
+        admission3_id = admission3_qs.filter(admission3=column[9]).filter(admission2_id=admission2_id) \
+            .filter(admission1_id=admission1_id).filter(univ_major_id=univ_major_id) \
+            .filter(univ_name_id=univ_name_id)[0]
+
+        if column[44] == '' or column[44] == '0':
+            column[44] = None
+
+        personnel_id = personnel_qs.filter(personnel=column[44]).filter(admission2_id=admission2_id) \
+            .filter(admission1_id=admission1_id).filter(univ_major_id=univ_major_id) \
+            .filter(univ_name_id=univ_name_id)[0]
+
+        if column[11] == '' or column[11] == '0':
+            column[11] = None
+
+        if column[40] == '' or column[40] == '0':
+            column[40] = None
+
+        if column[42] == '' or column[42] == '0':
+            column[42] = None
+
+        if column[43] == '' or column[43] == '0':
+            column[43] = None
+
+        _, created = Student.objects.update_or_create(
+            entrance_year=column[0],
+            student_region=column[1],
+            univ_group=univ_group_id,
+            univ_region=univ_region_id,
+            major_group=major_group_id,
+            univ_name=univ_name_id,
+            univ_major=univ_major_id,
+            admission1=admission1_id,
+            admission2=admission2_id,
+            admission3=admission3_id,
+            grade=column[10],
+            univ_score=column[11],
+            korean=column[12],
+            english=column[13],
+            mathematics=column[14],
+            society=column[15],
+            science=column[16],
+            all_subject_100=column[17],
+            all_subject_244=column[18],
+            all_subject_433=column[19],
+            all_subject_235=column[20],
+            ko_en_math_soc_sci_100=column[21],
+            ko_en_math_soc_sci_244=column[22],
+            ko_en_math_soc_sci_334=column[23],
+            ko_en_math_soc_sci_433=column[24],
+            ko_en_math_100=column[25],
+            ko_en_math_soc_100=column[26],
+            ko_en_math_soc_244=column[27],
+            ko_en_math_soc_334=column[28],
+            ko_en_math_soc_433=column[29],
+            ko_en_math_soc_370=column[30],
+            ko_en_soc_100=column[31],
+            ko_en_soc_244=column[32],
+            ko_en_math_sci_100=column[33],
+            ko_en_math_sci_244=column[34],
+            ko_en_math_sci_334=column[35],
+            ko_en_math_sci_433=column[36],
+            ko_en_math_sci_370=column[37],
+            en_math_sci_100=column[38],
+            en_math_sci_244=column[39],
+            first_step=column[40],
+            final_step=column[41],
+            fail_reason=column[42],
+            candidate_rank=column[43],
+            personnel=personnel_id
+        )
     context = {}
     return render(request, template, context)
--- config/urls.py
+++ config/urls.py
@@ -8,6 +8,7 @@ from analysis.views import univ_data_upload4
 from analysis.views import univ_data_upload5
 from analysis.views import univ_data_upload6
 from analysis.views import univ_data_upload7
+from analysis.views import univ_data_upload8
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -20,4 +21,5 @@ urlpatterns = [
     url(r'^upload-csv5/', univ_data_upload5, name='univ_data_upload5'),
     url(r'^upload-csv6/', univ_data_upload6, name='univ_data_upload6'),
     url(r'^upload-csv7/', univ_data_upload7, name='univ_data_upload7'),
+    url(r'^upload-csv8/', univ_data_upload8, name='univ_data_upload8'),
 ]
