9f86a44f55d7047f1c35bc28defa1fa46d92aea5
KYOUNGSOO-LEE <47710258+KYOUNGSOO-LEE@users.noreply.github.com>
2019-10-14 23:05:14 +0900

    19101410

--- analysis/forms.py
+++ analysis/forms.py
@@ -1,9 +1,19 @@
 from django import forms
 from crispy_forms.helper import FormHelper
-
 from .models import *
 
 
+class AdvancedForm(forms.Form):
+    helper = FormHelper()
+    helper.form_show_labels = False
+
+    major_group = forms.ModelChoiceField(queryset=MajorGroup.objects.all().order_by('major_group'), empty_label='계열')
+    univ_region = forms.ModelChoiceField(queryset=UnivRegion.objects.all().order_by('univ_region'), empty_label='지역')
+    univ_name = forms.ChoiceField()
+    univ_major = forms.ChoiceField()
+    admission1 = forms.ChoiceField()
+
+"""
 class AdvancedForm(forms.ModelForm):
     helper = FormHelper()
     helper.form_show_labels = False
@@ -16,3 +26,4 @@ class AdvancedForm(forms.ModelForm):
                   'univ_major',
                   'admission1',
         )
+"""
