5fcd5e03da2e81db77b381738ec8b1c9c7c9a5c3
KYOUNGSOO-LEE <47710258+KYOUNGSOO-LEE@users.noreply.github.com>
2019-10-20 02:23:40 +0900

    19102002

--- statistic/views.py
+++ statistic/views.py
@@ -7,6 +7,7 @@ def statistic1(request):
     template = "statistic/statistic1.html"
 
     univ_name_qs = UnivName.objects.all()
+    student_sum = Student.objects.all().count()
     univ_freq_qs = Student.objects.values_list('univ_name')\
                        .annotate(univ_count=Count('univ_name'))\
                        .order_by('-univ_count')[:10]
@@ -19,6 +20,7 @@ def statistic1(request):
         univ_freq_list.append(univ[1])
 
     context = {
+        'student_sum': student_sum,
         'univ_name': univ_name_list,
         'univ_freq': univ_freq_list,
     }
