111ad7126b5f004721fd4f93ef1eb2c71b714df7
KYOUNGSOO-LEE <47710258+KYOUNGSOO-LEE@users.noreply.github.com>
2019-10-20 02:04:28 +0900

    19102001

--- statistic/views.py
+++ statistic/views.py
@@ -1,14 +1,25 @@
 from django.shortcuts import render
 from analysis.models import *
+from django.db.models import Count
 
 
 def statistic1(request):
     template = "statistic/statistic1.html"
 
-    student_sum = Student.objects.all().count();
-    student_qs = Student.objects.filter();
+    univ_name_qs = UnivName.objects.all()
+    univ_freq_qs = Student.objects.values_list('univ_name')\
+                       .annotate(univ_count=Count('univ_name'))\
+                       .order_by('-univ_count')[:10]
+    univ_name_list = []
+    univ_freq_list = []
+
+    for univ in univ_freq_qs:
+        univ_name = univ_name_qs.get(pk=univ[0])
+        univ_name_list.append(univ_name.univ_name)
+        univ_freq_list.append(univ[1])
 
     context = {
-        'student_sum': student_sum,
+        'univ_name': univ_name_list,
+        'univ_freq': univ_freq_list,
     }
     return render(request, template, context)
