53673453441c996384856813731493dfb37fd496
KYOUNGSOO-LEE <47710258+KYOUNGSOO-LEE@users.noreply.github.com>
2019-10-20 13:05:20 +0900

    19102012

--- statistic/views.py
+++ statistic/views.py
@@ -6,6 +6,16 @@ from django.db.models import Count
 def statistic1(request):
     template = "statistic/statistic1.html"
 
+    chart0_student_region_freq_qs = Student.objects.values_list('student_region') \
+                              .annotate(student_region_count=Count('student_region')) \
+                              .order_by('-student_region_count')
+    chart0_student_region_list = []
+    chart0_student_region_freq_list = []
+
+    for chart0_student_region in chart0_student_region_freq_qs:
+        chart0_student_region_list.append(chart0_student_region[0])
+        chart0_student_region_freq_list.append(chart0_student_region[1])
+
     univ_name_qs = UnivName.objects.all()
 
     chart1_sum = Student.objects.all().count()
@@ -35,6 +45,8 @@ def statistic1(request):
         chart2_univ_freq_list.append(univ[1])
 
     context = {
+        'chart0_student_region_list': chart0_student_region_list,
+        'chart0_student_region_freq_list': chart0_student_region_freq_list,
         'chart1_sum': chart1_sum,
         'chart1_univ_name_list': chart1_univ_name_list,
         'chart1_univ_freq_list': chart1_univ_freq_list,
