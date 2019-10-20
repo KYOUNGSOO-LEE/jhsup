46fd7a66c774c3be4272bc94741668e8b943ff0b
KYOUNGSOO-LEE <47710258+KYOUNGSOO-LEE@users.noreply.github.com>
2019-10-20 11:41:15 +0900

    19102011

--- statistic/views.py
+++ statistic/views.py
@@ -7,21 +7,39 @@ def statistic1(request):
     template = "statistic/statistic1.html"
 
     univ_name_qs = UnivName.objects.all()
-    student_sum = Student.objects.all().count()
-    univ_freq_qs = Student.objects.values_list('univ_name')\
-                       .annotate(univ_count=Count('univ_name'))\
-                       .order_by('-univ_count')[:10]
-    univ_name_list = []
-    univ_freq_list = []
 
-    for univ in univ_freq_qs:
-        univ_name = univ_name_qs.get(pk=univ[0])
-        univ_name_list.append(univ_name.univ_name)
-        univ_freq_list.append(univ[1])
+    chart1_sum = Student.objects.all().count()
+    chart1_univ_freq_qs = Student.objects.values_list('univ_name')\
+                              .annotate(univ_count=Count('univ_name'))\
+                              .order_by('-univ_count')[:10]
+    chart1_univ_name_list = []
+    chart1_univ_freq_list = []
+
+    for univ in chart1_univ_freq_qs:
+        chart1_univ_name = univ_name_qs.get(pk=univ[0])
+        chart1_univ_name_list.append(chart1_univ_name.univ_name)
+        chart1_univ_freq_list.append(univ[1])
+
+
+    chart2_sum = Student.objects.filter(student_region='충남').count()
+    chart2_univ_freq_qs = Student.objects.filter(student_region='충남')\
+                              .values_list('univ_name')\
+                              .annotate(univ_count=Count('univ_name'))\
+                              .order_by('-univ_count')[:10]
+    chart2_univ_name_list = []
+    chart2_univ_freq_list = []
+
+    for univ in chart2_univ_freq_qs:
+        chart2_univ_name = univ_name_qs.get(pk=univ[0])
+        chart2_univ_name_list.append(chart2_univ_name.univ_name)
+        chart2_univ_freq_list.append(univ[1])
 
     context = {
-        'student_sum': student_sum,
-        'univ_name': univ_name_list,
-        'univ_freq': univ_freq_list,
+        'chart1_sum': chart1_sum,
+        'chart1_univ_name_list': chart1_univ_name_list,
+        'chart1_univ_freq_list': chart1_univ_freq_list,
+        'chart2_sum': chart2_sum,
+        'chart2_univ_name_list': chart2_univ_name_list,
+        'chart2_univ_freq_list': chart2_univ_freq_list,
     }
     return render(request, template, context)
