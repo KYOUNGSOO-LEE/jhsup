d271aa8aa3caca44c1170b433529166c55481f04
KYOUNGSOO-LEE <47710258+KYOUNGSOO-LEE@users.noreply.github.com>
2019-10-22 18:39:57 +0900

    19102218

--- statistic/urls.py
+++ statistic/urls.py
@@ -2,5 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^num_application$', views.num_application, name="num_application"),
+    url(r'^app-region$', views.app_region, name="app_region"),
+    url(r'^app-region/app-tab-dj$', views.app_tap_region_dj, name="app_tap_region_dj"),
+    url(r'^app-region/app-tab-sj$', views.app_tap_region_sj, name="app_tap_region_sj"),
+    url(r'^app-region/app-tab-cn$', views.app_tap_region_cn, name="app_tap_region_cn"),
+    url(r'^app-region/app-tab-cb$', views.app_tap_region_cb, name="app_tap_region_cb"),
 ]
--- statistic/views.py
+++ statistic/views.py
@@ -3,8 +3,8 @@ from analysis.models import *
 from django.db.models import Count
 
 
-def num_application(request):
-    template = "statistic/num_application.html"
+def app_region(request):
+    template = "statistic/app_region.html"
 
     # 지역별 사례수
     chart0_student_region_freq_qs = Student.objects.values_list('student_region') \
@@ -17,7 +17,7 @@ def num_application(request):
         chart0_student_region_list.append(chart0_student_region[0])
         chart0_student_region_freq_list.append(chart0_student_region[1])
 
-    # 대전, 세종, 충남, 충북지역 학생 지원대학 현황
+    # 충청도지역 학생 지원대학 현황
     univ_name_qs = UnivName.objects.all()
 
     chart1_sum = Student.objects.all().count()
@@ -32,6 +32,32 @@ def num_application(request):
         chart1_univ_name_list.append(chart1_univ_name.univ_name)
         chart1_univ_freq_list.append(univ[1])
 
+    context = {
+        'chart0_student_region_list': chart0_student_region_list,
+        'chart0_student_region_freq_list': chart0_student_region_freq_list,
+        'chart1_sum': chart1_sum,
+        'chart1_univ_name_list': chart1_univ_name_list,
+        'chart1_univ_freq_list': chart1_univ_freq_list,
+
+    }
+    return render(request, template, context)
+
+def app_tap_region_dj(request):
+    template = "statistic/app_region_tab_dj.html"
+
+    univ_name_qs = UnivName.objects.all()
+
+    # 지역별 사례수
+    chart0_student_region_freq_qs = Student.objects.values_list('student_region') \
+        .annotate(student_region_count=Count('student_region')) \
+        .order_by('-student_region_count')
+    chart0_student_region_list = []
+    chart0_student_region_freq_list = []
+
+    for chart0_student_region in chart0_student_region_freq_qs:
+        chart0_student_region_list.append(chart0_student_region[0])
+        chart0_student_region_freq_list.append(chart0_student_region[1])
+
     # 대전지역 학생 지원대학 현황
     application_dj_sum = Student.objects.filter(student_region='대전').count()
     application_univ_freq_dj_qs = Student.objects.filter(student_region='대전').values_list('univ_name').annotate(
@@ -43,6 +69,33 @@ def num_application(request):
         application_univ_name_dj = univ_name_qs.get(pk=univ[0])
         application_univ_name_dj_list.append(application_univ_name_dj.univ_name)
         application_univ_freq_dj_list.append(univ[1])
+
+    context = {
+        'chart0_student_region_list': chart0_student_region_list,
+        'chart0_student_region_freq_list': chart0_student_region_freq_list,
+
+        'application_dj_sum': application_dj_sum,
+        'application_univ_name_dj_list': application_univ_name_dj_list,
+        'application_univ_freq_dj_list': application_univ_freq_dj_list,
+    }
+    return render(request, template, context)
+
+
+def app_tap_region_sj(request):
+    template = "statistic/app_region_tab_sj.html"
+
+    # 지역별 사례수
+    chart0_student_region_freq_qs = Student.objects.values_list('student_region') \
+        .annotate(student_region_count=Count('student_region')) \
+        .order_by('-student_region_count')
+    chart0_student_region_list = []
+    chart0_student_region_freq_list = []
+
+    for chart0_student_region in chart0_student_region_freq_qs:
+        chart0_student_region_list.append(chart0_student_region[0])
+        chart0_student_region_freq_list.append(chart0_student_region[1])
+
+    univ_name_qs = UnivName.objects.all()
 
     # 세종지역 학생 지원대학 현황
     application_sj_sum = Student.objects.filter(student_region='세종').count()
@@ -56,6 +109,33 @@ def num_application(request):
         application_univ_name_sj_list.append(application_univ_name_sj.univ_name)
         application_univ_freq_sj_list.append(univ[1])
 
+    context = {
+        'chart0_student_region_list': chart0_student_region_list,
+        'chart0_student_region_freq_list': chart0_student_region_freq_list,
+
+        'application_sj_sum': application_sj_sum,
+        'application_univ_name_sj_list': application_univ_name_sj_list,
+        'application_univ_freq_sj_list': application_univ_freq_sj_list,
+    }
+    return render(request, template, context)
+
+
+def app_tap_region_cn(request):
+    template = "statistic/app_region_tab_cn.html"
+
+    # 지역별 사례수
+    chart0_student_region_freq_qs = Student.objects.values_list('student_region') \
+        .annotate(student_region_count=Count('student_region')) \
+        .order_by('-student_region_count')
+    chart0_student_region_list = []
+    chart0_student_region_freq_list = []
+
+    for chart0_student_region in chart0_student_region_freq_qs:
+        chart0_student_region_list.append(chart0_student_region[0])
+        chart0_student_region_freq_list.append(chart0_student_region[1])
+
+    univ_name_qs = UnivName.objects.all()
+
     # 충남지역 학생 지원대학 현황
     application_cn_sum = Student.objects.filter(student_region='충남').count()
     application_univ_freq_cn_qs = Student.objects.filter(student_region='충남').values_list('univ_name').annotate(
@@ -67,6 +147,33 @@ def num_application(request):
         application_univ_name_cn = univ_name_qs.get(pk=univ[0])
         application_univ_name_cn_list.append(application_univ_name_cn.univ_name)
         application_univ_freq_cn_list.append(univ[1])
+
+    context = {
+        'chart0_student_region_list': chart0_student_region_list,
+        'chart0_student_region_freq_list': chart0_student_region_freq_list,
+
+        'application_cn_sum': application_cn_sum,
+        'application_univ_name_cn_list': application_univ_name_cn_list,
+        'application_univ_freq_cn_list': application_univ_freq_cn_list,
+    }
+    return render(request, template, context)
+
+
+def app_tap_region_cb(request):
+    template = "statistic/app_region_tab_cb.html"
+
+    # 지역별 사례수
+    chart0_student_region_freq_qs = Student.objects.values_list('student_region') \
+        .annotate(student_region_count=Count('student_region')) \
+        .order_by('-student_region_count')
+    chart0_student_region_list = []
+    chart0_student_region_freq_list = []
+
+    for chart0_student_region in chart0_student_region_freq_qs:
+        chart0_student_region_list.append(chart0_student_region[0])
+        chart0_student_region_freq_list.append(chart0_student_region[1])
+
+    univ_name_qs = UnivName.objects.all()
 
     # 충북지역 학생 지원대학 현황
     application_cb_sum = Student.objects.filter(student_region='충북').count()
@@ -80,29 +187,12 @@ def num_application(request):
         application_univ_name_cb_list.append(application_univ_name_cb.univ_name)
         application_univ_freq_cb_list.append(univ[1])
 
-
     context = {
         'chart0_student_region_list': chart0_student_region_list,
         'chart0_student_region_freq_list': chart0_student_region_freq_list,
-        'chart1_sum': chart1_sum,
-        'chart1_univ_name_list': chart1_univ_name_list,
-        'chart1_univ_freq_list': chart1_univ_freq_list,
-
-        'application_dj_sum': application_dj_sum,
-        'application_univ_name_dj_list': application_univ_name_dj_list,
-        'application_univ_freq_dj_list': application_univ_freq_dj_list,
-
-        'application_sj_sum': application_sj_sum,
-        'application_univ_name_sj_list': application_univ_name_sj_list,
-        'application_univ_freq_sj_list': application_univ_freq_sj_list,
-
-        'application_cn_sum': application_cn_sum,
-        'application_univ_name_cn_list': application_univ_name_cn_list,
-        'application_univ_freq_cn_list': application_univ_freq_cn_list,
 
         'application_cb_sum': application_cb_sum,
         'application_univ_name_cb_list': application_univ_name_cb_list,
         'application_univ_freq_cb_list': application_univ_freq_cb_list,
-
     }
     return render(request, template, context)
