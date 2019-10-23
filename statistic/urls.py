7aaecd581b7934a2c2fce606ea352503e81f2b65
KYOUNGSOO-LEE <47710258+KYOUNGSOO-LEE@users.noreply.github.com>
2019-10-24 02:04:45 +0900

    19102402

--- statistic/urls.py
+++ statistic/urls.py
@@ -3,10 +3,12 @@ from . import views
 
 urlpatterns = [
     url(r'^app-region$', views.app_region, name="app_region"),
-    url(r'^app-region/app-tab-dj$', views.app_tap_region_dj, name="app_tap_region_dj"),
-    url(r'^app-region/app-tab-sj$', views.app_tap_region_sj, name="app_tap_region_sj"),
-    url(r'^app-region/app-tab-cn$', views.app_tap_region_cn, name="app_tap_region_cn"),
-    url(r'^app-region/app-tab-cb$', views.app_tap_region_cb, name="app_tap_region_cb"),
+    url(r'^app-region/app-tab-dj$', views.app_tab_region_dj, name="app_tab_region_dj"),
+    url(r'^app-region/app-tab-sj$', views.app_tab_region_sj, name="app_tab_region_sj"),
+    url(r'^app-region/app-tab-cn$', views.app_tab_region_cn, name="app_tab_region_cn"),
+    url(r'^app-region/app-tab-cb$', views.app_tab_region_cb, name="app_tab_region_cb"),
 
     url(r'^app-grade-ja$', views.app_grade_ja, name="app_grade_ja"),
+    url(r'^app-grade-ja/app-tab-ja-2$', views.app_tab_grade_ja_2, name="app_tab_grade_ja_2"),
+    url(r'^app-grade-ja/app-tab-ja-3$', views.app_tab_grade_ja_3, name="app_tab_grade_ja_3"),
 ]
--- statistic/views.py
+++ statistic/views.py
@@ -40,7 +40,7 @@ def app_region(request):
     }
     return render(request, template, context)
 
-def app_tap_region_dj(request):
+def app_tab_region_dj(request):
     template = "statistic/app_region_tab_dj.html"
 
     univ_name_qs = UnivName.objects.all()
@@ -78,7 +78,7 @@ def app_tap_region_dj(request):
     return render(request, template, context)
 
 
-def app_tap_region_sj(request):
+def app_tab_region_sj(request):
     template = "statistic/app_region_tab_sj.html"
 
     # 지역별 사례수
@@ -116,7 +116,7 @@ def app_tap_region_sj(request):
     return render(request, template, context)
 
 
-def app_tap_region_cn(request):
+def app_tab_region_cn(request):
     template = "statistic/app_region_tab_cn.html"
 
     # 지역별 사례수
@@ -154,7 +154,7 @@ def app_tap_region_cn(request):
     return render(request, template, context)
 
 
-def app_tap_region_cb(request):
+def app_tab_region_cb(request):
     template = "statistic/app_region_tab_cb.html"
 
     # 지역별 사례수
@@ -196,9 +196,10 @@ def app_grade_ja(request):
     template = "statistic/app_grade_ja.html"
 
     # 등급별 사례수
-    student_grade_freq_qs = Student.objects.values_list(Floor('ko_en_math_sci_100'))\
+    student_grade_freq_qs = Student.objects.filter(major_group=1)\
+        .values_list(Floor('ko_en_math_sci_100'))\
         .annotate(student_grade_count=Count(Floor('ko_en_math_sci_100')))\
-        .order_by('student_grade_count')
+        .order_by(Floor('ko_en_math_sci_100'))
     student_grade_ja_list = []
     student_grade_ja_freq_list = []
 
@@ -206,10 +207,108 @@ def app_grade_ja(request):
         student_grade_ja_list.append(int(student_grade[0]))
         student_grade_ja_freq_list.append(student_grade[1])
 
-    print(student_grade_ja_list)
+    # 자연 1등급대 학생 지원대학 현황
+    univ_name_qs = UnivName.objects.all()
+
+    application_1_sum = Student.objects.filter(ko_en_math_sci_100__lt=2).count()
+    application_univ_freq_1_qs = Student.objects.filter(ko_en_math_sci_100__lt=2).values_list('univ_name').annotate(
+        univ_count=Count('univ_name')).order_by('-univ_count')[:10]
+    application_univ_name_1_list = []
+    application_univ_freq_1_list = []
+
+    for univ in application_univ_freq_1_qs:
+        application_univ_name_1 = univ_name_qs.get(pk=univ[0])
+        application_univ_name_1_list.append(application_univ_name_1.univ_name)
+        application_univ_freq_1_list.append(univ[1])
 
     context = {
         'student_grade_ja_list': student_grade_ja_list,
         'student_grade_ja_freq_list': student_grade_ja_freq_list,
+
+        'application_1_sum': application_1_sum,
+        'application_univ_name_1_list': application_univ_name_1_list,
+        'application_univ_freq_1_list': application_univ_freq_1_list,
+    }
+    return render(request, template, context)
+
+
+def app_tab_grade_ja_2(request):
+    template = "statistic/app_grade_ja_tab_2.html"
+
+    # 등급별 사례수
+    student_grade_freq_qs = Student.objects.filter(major_group=1) \
+        .values_list(Floor('ko_en_math_sci_100')) \
+        .annotate(student_grade_count=Count(Floor('ko_en_math_sci_100'))) \
+        .order_by(Floor('ko_en_math_sci_100'))
+    student_grade_ja_list = []
+    student_grade_ja_freq_list = []
+
+    for student_grade in student_grade_freq_qs:
+        student_grade_ja_list.append(int(student_grade[0]))
+        student_grade_ja_freq_list.append(student_grade[1])
+
+    # 자연 2등급대 학생 지원대학 현황
+    univ_name_qs = UnivName.objects.all()
+
+    application_2_sum = Student.objects.filter(ko_en_math_sci_100__gte=2).filter(ko_en_math_sci_100__lt=3).count()
+    application_univ_freq_2_qs = Student.objects.filter(ko_en_math_sci_100__gte=2).filter(ko_en_math_sci_100__lt=3)\
+                                     .values_list('univ_name').annotate(univ_count=Count('univ_name'))\
+                                     .order_by('-univ_count')[:10]
+    application_univ_name_2_list = []
+    application_univ_freq_2_list = []
+
+    for univ in application_univ_freq_2_qs:
+        application_univ_name_2 = univ_name_qs.get(pk=univ[0])
+        application_univ_name_2_list.append(application_univ_name_2.univ_name)
+        application_univ_freq_2_list.append(univ[1])
+
+    context = {
+        'student_grade_ja_list': student_grade_ja_list,
+        'student_grade_ja_freq_list': student_grade_ja_freq_list,
+
+        'application_2_sum': application_2_sum,
+        'application_univ_name_2_list': application_univ_name_2_list,
+        'application_univ_freq_2_list': application_univ_freq_2_list,
+    }
+    return render(request, template, context)
+
+
+def app_tab_grade_ja_3(request):
+    template = "statistic/app_grade_ja_tab_3.html"
+
+    # 등급별 사례수
+    student_grade_freq_qs = Student.objects.filter(major_group=1) \
+        .values_list(Floor('ko_en_math_sci_100')) \
+        .annotate(student_grade_count=Count(Floor('ko_en_math_sci_100'))) \
+        .order_by(Floor('ko_en_math_sci_100'))
+    student_grade_ja_list = []
+    student_grade_ja_freq_list = []
+
+    for student_grade in student_grade_freq_qs:
+        student_grade_ja_list.append(int(student_grade[0]))
+        student_grade_ja_freq_list.append(student_grade[1])
+
+    # 자연 3등급대 학생 지원대학 현황
+    univ_name_qs = UnivName.objects.all()
+
+    application_3_sum = Student.objects.filter(ko_en_math_sci_100__gte=3).filter(ko_en_math_sci_100__lt=4).count()
+    application_univ_freq_3_qs = Student.objects.filter(ko_en_math_sci_100__gte=3).filter(ko_en_math_sci_100__lt=4)\
+                                     .values_list('univ_name').annotate(univ_count=Count('univ_name'))\
+                                     .order_by('-univ_count')[:10]
+    application_univ_name_3_list = []
+    application_univ_freq_3_list = []
+
+    for univ in application_univ_freq_3_qs:
+        application_univ_name_3 = univ_name_qs.get(pk=univ[0])
+        application_univ_name_3_list.append(application_univ_name_3.univ_name)
+        application_univ_freq_3_list.append(univ[1])
+
+    context = {
+        'student_grade_ja_list': student_grade_ja_list,
+        'student_grade_ja_freq_list': student_grade_ja_freq_list,
+
+        'application_3_sum': application_3_sum,
+        'application_univ_name_3_list': application_univ_name_3_list,
+        'application_univ_freq_3_list': application_univ_freq_3_list,
     }
     return render(request, template, context)
